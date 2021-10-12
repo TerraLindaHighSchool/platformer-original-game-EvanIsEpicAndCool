5760bf4ad7bd4499199bc988b47a35e4f0d18489
Evan Egan <ee42425@srcs.org>
2021-10-12 14:20:53 -0700

    finished project #6
    sorry I didnt do it earlier
    
    I forgor 💀

--- Level1.java
+++ Level1.java
@@ -8,6 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Level1 extends World
 {
+    
+    private final float GRAVITY = 0.0667f;
+    private final GreenfootSound MUSIC = new GreenfootSound("zapsplat_024.mp3");
     /**
      * Constructor for objects of class BrickWorld.
      * 
@@ -18,6 +21,12 @@ public class Level1 extends World
         super(1200, 800, 1, false); 
         prepare();
     }
+    
+    public void act()
+    {
+        spawn();
+    }
+    
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
@@ -25,9 +34,40 @@ public class Level1 extends World
     private void prepare()
     {
         setPaintOrder(Player.class, Platform.class, Obstacle.class, Collectable.class, Door.class, HUD.class);
-        Player player = new Player();
+        Player player = new Player(3, 5.6f, GRAVITY, 3, 3, Level2.class, MUSIC);
         addObject(player,31,762);
         Door door = new Door();
         addObject(door,1180,39);
+        Floor floor = new Floor();
+        addObject(new Floor(), 600, 800);
+        SmBrickWall smBrickWall = new SmBrickWall();
+        addObject(smBrickWall,814,584);
+        BrickWall brickWall = new BrickWall();
+        addObject(brickWall,512,437);
+        BrickWall brickWall2 = new BrickWall();
+        addObject(brickWall2,886,248);
+        SmBrickWall smBrickWall2 = new SmBrickWall();
+        addObject(smBrickWall2,125,288);
+        Gem gem = new Gem();
+        addObject(gem,90,248);
+        Gem gem2 = new Gem();
+        addObject(gem2,146,249);
+        Bomb bomb = new Bomb(GRAVITY);
+        addObject( new Bomb(GRAVITY),802,200);
+        Bomb bomb2 = new Bomb(GRAVITY);
+        addObject(bomb2,323,389);
+        TrapDoor trapDoor = new TrapDoor(GRAVITY);
+        addObject(trapDoor,995,467);
+        TrapDoor trapDoor2 = new TrapDoor(GRAVITY);
+        addObject(trapDoor2,354,169);
+        smBrickWall.setLocation(754,640);
+    }
+    
+    private void spawn()
+    {
+        if(Math.random() < 0.5)
+        {
+            addObject(new Rock(GRAVITY), Greenfoot.getRandomNumber(1200), -30);
+        }
     }
 }
